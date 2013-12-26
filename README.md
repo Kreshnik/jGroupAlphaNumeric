f2bf44ee633fa95542cff612882874b23412ad4b
Kreshnik Hasanaj <kreshnik.hasanaj@gmail.com>
2013-12-26 22:56:53 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,25 @@
+#jGroupAlphaNumeric
+
+###This plugin helps you group items into alpha numeric groups, to make use of it you have to specify one or more characters on which the items will be grouped. 
+
+
+#####You have to specify for each element you want to group a data attribute named as default "group-character"
+`<a data-group-character="a" href="javascript:void(0);" title="">Letter A</a>`
+
+#####If you look into the source code you can see that you can change few of these options, for the plugin.
+
+    {
+        dataSelector: "group-character", // The default data attribute that is holding the group character
+        wrapperTag: "div", // The default wrap tag
+        addHeader: true, // Specify wherever you want to add a header to each group or not
+        headerTag: "h1" // Specify the header tag
+    }
+
+#####To initialize the function you just called it like this
+
+    $(document).ready(function(){
+		$.groupAlphaNumeric();
+	});
+
+Also here you have a image preview after jGroupAlphaNumeric has been called.
+![Alt text](preview.png "Image preview")
---
+++ jGroupAlphaNumeric.js
@@ -0,0 +1,108 @@
+if( typeof Object.create !== 'function') {
+    Object.create = function(obj){
+        function F(){};
+        F.prototype = obj;
+        return new F(); 
+    }
+}
+(function ( $, window, document, undefined ) {
+    var groupAlphaNumeric = {
+        init: function(options){
+            var self = this;
+            self.elements;
+            self.groups = [];
+            self.finalGroups = [];
+            self.options = $.extend({}, $.groupAlphaNumeric.options, options);
+            self.group();
+        },
+        getElementList: function(){
+            
+            this.elements = $("[data-" + this.options.dataSelector + "]");
+            this.getGroups();
+            
+        },
+        getGroups: function(){
+            
+            if( this.elements.length > 0 ){
+                var self = this;
+                this.elements.map(function(){
+                     if($.inArray( $(this).data(self.options.dataSelector) , self.groups) === -1){
+                         self.groups.push( $(this).data(self.options.dataSelector) );
+                     }
+                 });
+                 this.getSorted();
+                
+            } else {
+                
+                return null;
+                
+            } 
+        },
+        getSorted: function(){
+            
+            if( this.elements.length > 0 ){
+                this.finalGroups = this.groups.sort();
+                this.wrapGroups();
+            } else {
+                
+                return null;
+                
+            } 
+        },
+        wrapGroups: function(){
+            
+             if( this.finalGroups.length > 0 ){
+                var self = this;
+                this.finalGroups.forEach(function(elem){
+                     var wrapElement = document.createElement( self.options.wrapperTag );
+                     wrapElement.setAttribute('class', 'group-alphanumeric group-' + elem);
+                     wrapElement.setAttribute('data-group-header', elem);
+                     var selectedGroup = $("[data-" + self.options.dataSelector + "='" + elem + "']");
+                     selectedGroup.wrapAll( wrapElement );
+                });
+                 
+                  this.addHeader();
+                 
+                 
+             } else {
+                 return null;
+             }         
+            
+        },
+        addHeader: function(){
+            if(this.options.addHeader === true){
+                var self = this;
+                $("[data-group-header]").each(function(index, elem){
+                    var $self = $(elem);
+                    var headerElement = document.createElement( self.options.headerTag );
+                    headerElement.innerText = $self.data('group-header');
+                    $self.prepend( headerElement );
+                    
+                });
+            }
+        },
+        group: function(){
+        
+            this.getElementList();
+            
+        }
+    };
+    
+    $.groupAlphaNumeric = function(options){    
+        if(this !== undefined){
+            var objMain = Object.create( groupAlphaNumeric );   
+            objMain.init(options);
+        } else {
+            return null;
+        }
+    }; 
+    
+    $.groupAlphaNumeric.options = {
+        dataSelector: "group-character",
+        wrapperTag: "div",
+        addHeader: true,
+        headerTag: "h1"
+    };
+ 
+}
+( jQuery, window, document ));
